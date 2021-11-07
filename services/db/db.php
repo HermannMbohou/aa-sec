05c8a183f6c98e47ff08cc970cd58802616b651e
Hermann Mbohou <45997417+HermannMbohou@users.noreply.github.com>
2021-11-07 15:28:09 +0100

    Update db.php

--- services/db/db.php
+++ services/db/db.php
@@ -5,7 +5,7 @@
         private $db;
 
         function __construct(){ 
-            $this->db = new PDO('mysql:host=https://www.db4free.net/;dbname=cotisons1234','admincotisons','iyckybEj7CKqp9');
+            $this->db = new PDO('mysql:host=db4free.net;dbname=cotisons1234','admincotisons','iyckybEj7CKqp9');
             $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
             $this->db->exec('SET NAMES utf8');
         }
